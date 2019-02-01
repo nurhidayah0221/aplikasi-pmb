03050fe12c93b2bd2c74acf675b57bda157c560f
nurhidayah0221 <nurhidayah0221@gmail.com>
2019-02-01 14:10:23 +0700

    perubahan tampilan login

---
+++ src/main/java/com/nurhidayah/aplikasipmb/AplikasiPmbApplication.java
@@ -0,0 +1,14 @@
+package com.nurhidayah.aplikasipmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiPmbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiPmbApplication.class, args);
+	}
+
+}
+
